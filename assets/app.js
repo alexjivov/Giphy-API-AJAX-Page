278eac38b11b5086d930fe7a57fdc51780d6f2f4
Alex Jivov <alexjivov@gwu.edu>
2018-06-28 22:08:03 -0400

    adding jquery plug-in for pausing/starting

--- assets/app.js
+++ assets/app.js
@@ -1,49 +1,56 @@
-// event listener for all button elements
-    $("button").on("click", function() {
-      // In this case, the "this" keyword refers to the button that clicked
-      var tvShow = $(this).attr("data-person");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-       tvShow + "&api_key=dc6zaTOxFJmzC&limit=10";
+/* Whats left to do
+1. Attach the classes to the .item class so data-still and animate work
+2. figure out how to create the add buttons without using an array */
 
-      $.ajax({
+
+// event listener for all button elements
+$("button").on("click", function () {
+    // In this case, the "this" keyword refers to the button that clicked
+    var tvShow = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        tvShow + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
         url: queryURL,
         method: "GET"
-      })
-        .then(function(response) {
-          //Storing an array of result sin the results variable
-          var topics = response.data;
-          // Looping over every result item
-          for (var i = 0; i < topics.length; i++) {
-            var gifDiv = $("<div class='item'>");
+    })
+        .then(function (response) {
+            //Storing an array of result sin the results variable
+            var topics = response.data;
+            
+            // Looping over every result item
+            for (var i = 0; i < topics.length; i++) {
+                var gifDiv = $("<div class='item'>");
+                var rating = topics[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var tvImage = $("<img>");
+                tvImage.attr("src", topics[i].images.original_still.url);
+                tvImage.attr("data-gif", topics[i].images.fixed_height.url);
+                console.log(topics);
+                gifDiv.prepend(p);
+                gifDiv.prepend(tvImage);
 
-            var rating = topics[i].rating;
+                $("#gifShow").prepend(gifDiv);
+            }
 
-            var p = $("<p>").text("Rating: " + rating);
+            // add the pause button using jQuery plugin - have to nest it in this function so it attaches
+            $('.item').gifplayer();
 
-            var tvImage = $("<img>");
-            tvImage.attr("src", topics[i].images.fixed_height.url);
-
-            gifDiv.prepend(p);
-            gifDiv.prepend(tvImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
         });
-    });
 
-    $(".item").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
+});
+
+// dynamically add the buttons for a new show when you enter into the search field
+function addButton() {
+
+    $("#gif-btn").on("click", function (event) {
+        event.preventDefault();
+
+        var newTopic = $("#giphy-input").val().trim();
+        newTopics.push(newTopic);
+        console.log("new topic: " + newTopic);
+        console.log("topics array: " + topics);
+        createButtons();
+        $("#giphy-input").val("");
     });
-    
-  
+}
